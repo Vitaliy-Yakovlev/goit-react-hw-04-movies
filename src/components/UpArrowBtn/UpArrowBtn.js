30c43594c505eaa3f5c11ac31436ccc9516eafa6
Vitaliy-Yakovlev <vitaliy-yakovlev@ukr.net>
2021-09-15 21:47:31 +0300

    Переделал кнопку вверх

--- src/components/UpArrowBtn/UpArrowBtn.js
+++ src/components/UpArrowBtn/UpArrowBtn.js
@@ -1,7 +1,16 @@
+import { useEffect, useState } from 'react';
 import { VscChevronUp } from 'react-icons/vsc';
 import s from './UpArrowBtn.module.css';
 
 function UpArrowBtn() {
+  const [scrollTop, setScrollTop] = useState(0);
+
+  useEffect(() => {
+    window.onscroll = () => {
+      setScrollTop(window.pageYOffset);
+    };
+  }, []);
+
   const scrollFunction = () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollIntoView({
@@ -10,24 +19,19 @@ function UpArrowBtn() {
     });
   };
 
-  //не работает!
-
-  const trackScroll = () => {
+  const className = () => {
     const optionClassName = [s.myBtn];
 
-    if (document.body.scrollTop === 200) {
+    if (scrollTop > 400) {
       optionClassName.push(s.isHidden);
-    }
 
+      return optionClassName.join(' ');
+    }
     return optionClassName.join(' ');
   };
 
   return (
-    <button
-      onClick={scrollFunction}
-      title="Go to top"
-      className={trackScroll()}
-    >
+    <button onClick={scrollFunction} title="Go to top" className={className()}>
       <VscChevronUp />
     </button>
   );
