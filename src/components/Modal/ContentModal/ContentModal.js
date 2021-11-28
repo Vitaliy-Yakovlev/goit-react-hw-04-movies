811753509d26c0fd23cfc3e1078e9542dbf0e44a
Vitaliy-Yakovlev <vitaliy-yakovlev@ukr.net>
2021-11-28 21:17:46 +0200

    Адаптировал видео и модалку

--- src/components/Modal/ContentModal/ContentModal.js
+++ src/components/Modal/ContentModal/ContentModal.js
@@ -5,17 +5,18 @@ export default function ContentModal({ video, VideoIdEnglish }) {
     <div className={s.containerVideos}>
       {video?.results?.length > 0 ? (
         <iframe
+          className={s.iframe}
           title="Video"
           src={`https://www.youtube.com/embed/${video?.results[0]?.key}`}
-          width="800"
-          height="600"
+          // width="800"
+          // height="600"
         ></iframe>
       ) : (
         <iframe
           title="Video"
           src={`https://www.youtube.com/embed/${VideoIdEnglish?.results[0]?.key}`}
-          width="800"
-          height="600"
+          // width="800"
+          // height="600"
         ></iframe>
       )}
     </div>
